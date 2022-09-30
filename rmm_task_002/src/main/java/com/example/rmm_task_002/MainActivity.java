0dd387ab10d21bfc5551d0b097b41545d0bab882
Ramil <r.mudarisov@d-1.space>
2022-10-01 00:03:02 +0600

    [NVA-001] - First commit

---
+++ rmm_task_002/src/main/java/com/example/rmm_task_002/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rmm_task_002;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
